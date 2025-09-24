78ba3db253dc8003d7a6f394c7533376803e6bc1
shanerone <gehbect@gmail.com>
2025-09-24 18:04:30 +0500

    seredina

--- laba1/1-3.cpp
+++ laba1/1-3.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 int charToNum (char x) {
-
+    return x - '0';
 }
 
 int main() {
+    char x;
+    cout << "Enter num: ";
+    cin >> x;
+    cout << charToNum(x) << endl;
     return 0;
 }
