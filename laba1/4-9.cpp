cf5528e798a52dd3081289a4ff3cf2070273ff49
shanerone <gehbect@gmail.com>
2025-09-25 17:43:11 +0500

    100%

--- laba1/4-9.cpp
+++ laba1/4-9.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 using namespace std;
-
-int * deleteNegative (int arr[], int size) {
+//параметр-ссылка, который позволяет функции
+// напрямую менять переменную, переданную в неё снаружи.
+int* findAll(int arr[], int size, int x, int &resultSize) {
+    int ind = 0;
     for (int i = 0; i < size; i++) {
-
+        if (arr[i] == x) ind++;
     }
+    resultSize = ind;
+    int* newArr = new int[ind];
+    int pos = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == x) {
+            newArr[pos++] = i;
+        }
+    }
+    return newArr;
 }
 
 int main() {
-    int size;
-    cout << "Enter num: ";
+    int size, x;
+    cout << "Enter size: ";
     if (!(cin >> size)) {
+        cerr << "incorrect" << endl;
+        return 1;
+    }
+    cout << "Enter x: ";
+    if (!(cin >> x)) {
         cerr << "incorrect" << endl;
         return 1;
     }
@@ -19,7 +35,16 @@ int main() {
     for (int i = 0; i < size; i++) {
         if (!(cin >> arr[i])) {
             cout << "Ошибка ввода" << endl;
+            delete[] arr;
             return 1;
         }
     }
+
+    int resultSize;
+    int* res = findAll(arr, size, x, resultSize);
+    for (int i = 0; i < resultSize; i++) {
+        cout << res[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
