4c4794a7eaf210bff8b4772bd00f1359f5e9a4e7
shanerone <gehbect@gmail.com>
2025-10-08 10:41:17 +0500

    2

--- laba2/zad2.cpp
+++ laba2/zad2.cpp
@@ -1,29 +1,50 @@
 #include <iostream>
 #include <vector>
 #include <deque>
-
 using namespace std;
 
 int main() {
     vector<int> V = {1, 2, 3, 4};
     deque<int> D = {5, 6, 7, 8};
 
-    // Добавляем в конец V первую половину D
-    int halfD = D.size() / 2;
-    V.insert(V.end(), D.begin(), D.begin() + halfD);
-
-    // Добавляем в начало D вторую половину V, но в обратном порядке
-    int halfV = V.size() / 2;
-    vector<int> tmp(V.begin() + halfV, V.end());
-    reverse(tmp.begin(), tmp.end());
-    D.insert(D.begin(), tmp.begin(), tmp.end());
-
-    // Выводим результат
+    cout << "Orig;" << endl;
     cout << "V: ";
     for (int x : V) cout << x << " ";
     cout << endl;
     cout << "D: ";
     for (int x : D) cout << x << " ";
     cout << endl;
+
+    vector<int> secondHalfV;
+    for (int i = V.size() / 2; i < V.size(); i++) {
+        secondHalfV.push_back(V[i]);
+    }
+
+    vector<int> reversedSecondHalf;
+    for (int i = secondHalfV.size() - 1; i >= 0; i--) {
+        reversedSecondHalf.push_back(secondHalfV[i]);
+    }
+
+    for (int i = 0; i < D.size() / 2; i++) {
+        V.push_back(D[i]);
+    }
+
+    deque<int> newD;
+    for (int x : reversedSecondHalf) {
+        newD.push_back(x);
+    }
+    for (int x : D) {
+        newD.push_back(x);
+    }
+    D = newD;
+
+    cout << "Done:" << endl;
+    cout << "V: ";
+    for (int x : V) cout << x << " ";
+    cout << endl;
+    cout << "D: ";
+    for (int x : D) cout << x << " ";
+    cout << endl;
+
     return 0;
 }
