a67fe9fa77a74db2dc54e81d31341987dda5adea
shanerone <gehbect@gmail.com>
2025-10-08 13:27:51 +0500

    2

--- laba2/zad2.cpp
+++ laba2/zad2.cpp
@@ -4,17 +4,39 @@
 using namespace std;
 
 int main() {
-    vector<int> V = {1, 2, 3, 4};
-    deque<int> D = {5, 6, 7, 8};
+    vector<int> V;
+    deque<int> D;
+    int sizeV;
+    cout << "enter size V: ";
+    if (!(cin >> sizeV)) {
+        cout << "incorrect" << endl;
+        return 1;
+    }
+    cout << "enter elements V: ";
+    for (int i = 0; i < sizeV; i++) {
+        int elem;
+        if (!(cin >> elem)) {
+            cout << "incorrect" << endl;
+            return 1;
+        }
+        V.push_back(elem);
+    }
 
-    cout << "Orig;" << endl;
-    cout << "V: ";
-    for (int x : V) cout << x << " ";
-    cout << endl;
-    cout << "D: ";
-    for (int x : D) cout << x << " ";
-    cout << endl;
-
+    int sizeD;
+    cout << "enter size D: ";
+    if (!(cin >> sizeD)) {
+        cout << "incorrect" << endl;
+        return 1;
+    }
+    cout << "enter elements D: ";
+    for (int i = 0; i < sizeD; i++) {
+        int elem;
+        if (!(cin >> elem)) {
+            cout << "incorrect" << endl;
+            return 1;
+        }
+        D.push_back(elem);
+    }
     vector<int> secondHalfV;
     for (int i = V.size() / 2; i < V.size(); i++) {
         secondHalfV.push_back(V[i]);
