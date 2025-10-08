f829862bcec1402df93ebebdd548e92e2bbeb864
shanerone <gehbect@gmail.com>
2025-10-08 13:36:31 +0500

    3

--- laba2/zad3.cpp
+++ laba2/zad3.cpp
@@ -2,10 +2,26 @@
 #include <vector>
 
 int main() {
-    std::vector<int> V = {1, 2, 3, 4, 5, 6, 7};
+    std::vector<int> V;
+    int size;
+    std::cout << "enter size: ";
+    if (!(std::cin >> size)) {
+        std::cout << "incorrect" << std::endl;
+        return 1;
+    }
+    std::cout << "enter elements: ";
+    for (int i = 0; i < size; i++) {
+        int elem;
+        if (!(std::cin >> elem)) {
+            std::cout << "incorrect" << std::endl;
+            return 1;
+        }
+        V.push_back(elem);
+    }
+
     std::cout << "original" << std::endl;
     for (int i = 0; i < V.size(); i++) {
-        std::cout << V[i];
+        std::cout << V[i] << " ";
     }
     std::cout << std::endl;
     int middle = V.size() / 2;
@@ -13,7 +29,7 @@ int main() {
 
     std::cout << "after removing three middle elements" << std::endl;
     for (int i = 0; i < V.size(); i++) {
-        std::cout << V[i];
+        std::cout << V[i] << " ";
     }
     std::cout << std::endl;
 }
