f589a97bf5eaff8843c9d7405adad7aa37fb0b78
Matvey Kaigorodcev <gehbect@gmail.com>
2025-11-27 14:11:53 +0500

    2 zad

--- laba4/2/main.cpp
+++ laba4/2/main.cpp
@@ -1,9 +1,6 @@
 #include "Time.h"
 
 int main() {
-    cout << "=== Тестирование класса Time с операторами ===\n\n";
-    
-    // Создаем тестовые объекты
     Time t1(14, 30);
     Time t2(9, 45);
     Time t3(0, 0);
@@ -13,59 +10,47 @@ int main() {
     cout << "t2 = " << t2.toString() << "\n";
     cout << "t3 = " << t3.toString() << "\n\n";
     
-    // === УНАРНЫЕ ОПЕРАЦИИ ===
-    cout << "=== УНАРНЫЕ ОПЕРАЦИИ ===\n\n";
-    
-    // Префиксный ++
     cout << "Префиксный ++:\n";
     cout << "t1 до: " << t1.toString() << "\n";
     ++t1;
     cout << "t1 после ++t1: " << t1.toString() << "\n\n";
     
-    // Постфиксный ++
     cout << "Постфиксный ++:\n";
     cout << "t2 до: " << t2.toString() << "\n";
     Time temp = t2++;
     cout << "t2 после t2++: " << t2.toString() << "\n";
     cout << "Возвращенное значение (t2++): " << temp.toString() << "\n\n";
     
-    // Префиксный --
     cout << "Префиксный --:\n";
     cout << "t1 до: " << t1.toString() << "\n";
     --t1;
     cout << "t1 после --t1: " << t1.toString() << "\n\n";
     
-    // Постфиксный --
     cout << "Постфиксный --:\n";
     cout << "t2 до: " << t2.toString() << "\n";
     Time temp2 = t2--;
     cout << "t2 после t2--: " << t2.toString() << "\n";
     cout << "Возвращенное значение (t2--): " << temp2.toString() << "\n\n";
     
-    // Тест на переход через границы
-    cout << "Тест переходов через границы:\n";
+    cout << "Переход в новый день:\n";
     Time t4(23, 59);
     cout << "t4 = " << t4.toString() << "\n";
     ++t4;
-    cout << "После ++: " << t4.toString() << " (должно быть 00:00)\n\n";
+    cout << "После ++: " << t4.toString() << "\n";
     
     Time t5(0, 0);
     cout << "t5 = " << t5.toString() << "\n";
     --t5;
-    cout << "После --: " << t5.toString() << " (должно быть 23:59)\n\n";
+    cout << "После --: " << t5.toString() << "\n";
     
-    // === ПРИВЕДЕНИЕ ТИПОВ ===
-    cout << "=== ПРИВЕДЕНИЕ ТИПОВ ===\n\n";
     
     Time t6(10, 15);
     Time t7(0, 0);
     
-    // Неявное приведение к int
-    cout << "Неявное приведение к int (количество минут):\n";
+    cout << "приведение к int (количество минут):\n";
     cout << "t6 = " << t6.toString() << " = " << (int)t6 << " минут\n";
     cout << "t7 = " << t7.toString() << " = " << (int)t7 << " минут\n\n";
     
-    // Явное приведение к bool
     cout << "Явное приведение к bool:\n";
     if (t6) {
         cout << "t6 = " << t6.toString() << " -> true (не нулевое время)\n";
@@ -75,8 +60,7 @@ int main() {
     }
     cout << "\n";
     
-    // === БИНАРНЫЕ ОПЕРАЦИИ ===
-    cout << "=== БИНАРНЫЕ ОПЕРАЦИИ ===\n\n";
+    cout << "Бинарные операции\n\n";
     
     Time t8(15, 30);
     Time t9(10, 45);
@@ -87,32 +71,21 @@ int main() {
     cout << "t9 = " << t9.toString() << "\n";
     cout << "t10 = " << t10.toString() << "\n\n";
     
-    // Оператор <
     cout << "Оператор <:\n";
     if (t9 < t8) {
         cout << t9.toString() << " < " << t8.toString() << " -> true\n";
-    }
-    if (t8 < t9) {
-        cout << t8.toString() << " < " << t9.toString() << " -> true\n";
     } else {
-        cout << t8.toString() << " < " << t9.toString() << " -> false\n";
+        cout << t9.toString() << " < " << t8.toString() << " -> false\n";
     }
     cout << "\n";
     
-    // Оператор >
     cout << "Оператор >:\n";
     if (t8 > t9) {
         cout << t8.toString() << " > " << t9.toString() << " -> true\n";
-    }
-    if (t8 > t10) {
-        cout << t8.toString() << " > " << t10.toString() << " -> true\n";
     } else {
-        cout << t8.toString() << " > " << t10.toString() << " -> false\n";
+        cout << t8.toString() << " > " << t9.toString() << " -> false\n";
     }
     cout << "\n";
-    
-    // === ИНТЕРАКТИВНОЕ ТЕСТИРОВАНИЕ ===
-    cout << "=== ИНТЕРАКТИВНОЕ ТЕСТИРОВАНИЕ ===\n\n";
     
     short int h, m;
     cout << "Введите часы (0-23): ";
