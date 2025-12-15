f82b20a931285129c1251c50e307f3a7d3b3330c
Matvey Kaigorodcev <gehbect@gmail.com>
2025-12-15 20:04:16 +0500

    zad1 + readme

--- laba5/1/za1.cpp
+++ laba5/1/za1.cpp
@@ -2,82 +2,65 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
-#include <climits>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-// Метод для заполнения бинарного файла случайными данными
-void fillFileWithRandomData(const string& filename, int count) {
+void fill_random(const string& filename, int count) {
     ofstream file(filename, ios::binary);
-    
-    if (!file.is_open()) {
-        cerr << "Ошибка: не удалось открыть файл для записи!" << endl;
-        return;
-    }
-    
-    srand(time(nullptr)); // Инициализация генератора случайных чисел
-    
+
+    srand(time(0));
+
     for (int i = 0; i < count; i++) {
-        int randomNumber = rand() % 1000; // Генерируем числа от 0 до 999
-        file.write(reinterpret_cast<const char*>(&randomNumber), sizeof(int));
+        int number = rand() % 100;
+        file.write((char*)&number, sizeof(int)); //4 байта, char работает с байтами
     }
-    
+
     file.close();
-    cout << "Файл успешно заполнен " << count << " случайными числами." << endl;
 }
 
-// Функция для нахождения разности максимального и минимального элементов
-int findDifference(const string& filename) {
+void find_raznost(const string& filename) {
     ifstream file(filename, ios::binary);
-    
-    if (!file.is_open()) {
-        cerr << "Ошибка: не удалось открыть файл для чтения!" << endl;
-        return -1;
+
+    if (!file) {
+        cout << "Error opening file";
+        return;
     }
-    
-    int minValue = INT_MAX;
-    int maxValue = INT_MIN;
+
     int number;
-    
-    // Читаем числа из файла и находим min и max
-    while (file.read(reinterpret_cast<char*>(&number), sizeof(int))) {
-        if (number < minValue) {
-            minValue = number;
-        }
-        if (number > maxValue) {
-            maxValue = number;
-        }
+    int maxVal = INT_MIN;
+    int minVal = INT_MAX;
+    int count = 0;
+
+    while (file.read((char*)&number, sizeof(int))) {
+        maxVal = max(maxVal, number);
+        minVal = min(minVal, number);
+        count++;
     }
-    
+
     file.close();
-    
-    if (minValue == INT_MAX || maxValue == INT_MIN) {
-        cerr << "Ошибка: файл пуст!" << endl;
-        return -1;
+
+    if (count == 0) {
+        cout << "Файл пуст!\n";
+        return;
     }
-    
-    cout << "Минимальный элемент: " << minValue << endl;
-    cout << "Максимальный элемент: " << maxValue << endl;
-    
-    return maxValue - minValue;
+
+    int difference = maxVal - minVal;
+
+    cout << "Max: " << maxVal << "\n";
+    cout << "Min: " << minVal << "\n";
+    cout << "Diff: " << difference << "\n";
 }
 
 int main() {
-    const string filename = "data.bin";
-    const int dataCount = 20; // Количество случайных чисел для генерации
-    
-    // Заполняем файл случайными данными
-    fillFileWithRandomData(filename, dataCount);
-    
-    // Находим разность максимального и минимального элементов
-    int difference = findDifference(filename);
-    
-    if (difference != -1) {
-        cout << "Разность максимального и минимального элементов: " << difference << endl;
-    }
-    
+    string filename = "data.bin";
+    int elementsCount;
+
+    cout << "Enter number of el: ";
+    cin >> elementsCount;
+
+    fill_random(filename, elementsCount);
+    find_raznost(filename);
+
     return 0;
 }
-
-
