6a074210725abe07fd4e7d8a4c071c65b9f02a32
Matvey Kaigorodcev <gehbect@gmail.com>
2025-12-16 19:26:25 +0500

    2 zad + readme

---
+++ laba5/2/za2.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
+using namespace std;
+
+void fill_random(const string& filename, int count) {
+    ofstream file(filename, ios::binary);
+    srand(time(0));
+    
+    for (int i = 0; i < count; i++) {
+        int number = rand() % 100;
+        file.write((char*)&number, sizeof(int));
+    }
+    
+    file.close();
+}
+
+void matrixFromFile(const string& filename, int n) {
+    ifstream file(filename, ios::binary);
+    
+    int numbers[1000];
+    int count = 0;
+    int number;
+    
+    while (file.read((char*)&number, sizeof(int)) && count < 1000) {
+        numbers[count] = number;
+        count++;
+    }
+    
+    file.close();
+    
+    int matrix[100][100] = {0};
+    
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (index < count) {
+                matrix[i][j] = numbers[index++];
+            }
+        }
+    }
+    
+    int maxVal = matrix[0][0];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            maxVal = max(maxVal, matrix[i][j]);
+        }
+    }
+    
+    cout << "\nMax element: " << maxVal << "\n";
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] == maxVal) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    
+    cout << "\nMatrix " << n << "x" << n << ":\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    int n, count;
+        
+        cout << "Enter size matrix (n x n): ";
+        cin >> n;
+        
+        cout << "Enter count numbers in file: ";
+        cin >> count;
+        
+        fill_random("data2.bin", count);
+        matrixFromFile("data2.bin", n);
+        
+        return 0;
+}
